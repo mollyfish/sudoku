6595f866e86897381f86177850544342c54d5584
Molly Kent <kent.molly@gmail.com>
2015-05-20 11:02:07 -0700

    layout and ids set up correctly, selected behavior working

--- public/js/sudoku.js
+++ public/js/sudoku.js
@@ -3,12 +3,26 @@ $(function(){
 
   $('.tile').click(function() {
         $('.tile').removeClass('selected');
+        $('.choice').removeClass('chosen');
         $(this).toggleClass('selected', this.clicked);
+        console.log($(this).attr('class') + ' ' + $(this).attr('id'));
     });
   $('.choice').click(function() {
     if ($('.tile').hasClass('selected')) {
-        $('.choice').removeClass('selected');
-        $(this).toggleClass('selected', this.clicked);
+        // console.log($('.selected').attr('id'));
+        $('.choice').removeClass('chosen');
+        $(this).toggleClass('chosen', this.clicked);
+        // console.log($(this).attr('id'));
+        // console.log($('.selected').html()); 
+        console.log($('.selected').attr('class'));
+        console.log($('.selected').siblings().length);
+        $('.selected').siblings().each(function(i) {
+          console.log( (i + 1) + ': ' + $(this).attr('id') + ' ' + $(this).html() + ' ' + $(this).attr('class'));
+        });
+
+
+        $('.selected').html($(this).html());
+        // console.log($('.selected').html()); 
       } else {
         alert('Please select a spot to place your tile');
       }
