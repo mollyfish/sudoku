1f7b904245bb09cae832d94fa2e236280118af97
Molly Kent <kent.molly@gmail.com>
2015-05-20 11:14:45 -0700

    began trying to check against matches, indentation problem is throwing a bug

--- public/js/sudoku.js
+++ public/js/sudoku.js
@@ -1,28 +1,37 @@
 'use strict';
 $(function(){
-
+var placeHere;
   $('.tile').click(function() {
         $('.tile').removeClass('selected');
         $('.choice').removeClass('chosen');
         $(this).toggleClass('selected', this.clicked);
         console.log($(this).attr('class') + ' ' + $(this).attr('id'));
+        placeHere = $(this).attr('id');
+        console.log('placeHere upon selection: ' + placeHere);
     });
   $('.choice').click(function() {
     if ($('.tile').hasClass('selected')) {
         // console.log($('.selected').attr('id'));
+        console.log('placeHere at beginning of choice: ' + placeHere);
         $('.choice').removeClass('chosen');
         $(this).toggleClass('chosen', this.clicked);
-        // console.log($(this).attr('id'));
-        // console.log($('.selected').html()); 
         console.log($('.selected').attr('class'));
         console.log($('.selected').siblings().length);
+        var siblings = []
+        $('.selected').siblings().each(function(i) {
+          siblings.push($(this).html());
+        };
+        console.log(siblings);
+
+        $('.selected').html($(this).html());
+
         $('.selected').siblings().each(function(i) {
           console.log( (i + 1) + ': ' + $(this).attr('id') + ' ' + $(this).html() + ' ' + $(this).attr('class'));
         });
 
 
-        $('.selected').html($(this).html());
-        // console.log($('.selected').html()); 
+        
+
       } else {
         alert('Please select a spot to place your tile');
       }
